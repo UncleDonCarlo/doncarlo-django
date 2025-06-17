6f034a496292b6ceb8b84b398bc5b2eeb191d7d2
Phakapon Philaphak <plam65731@gmail.com>
2025-06-17 18:30:34 +0700

    Patch : change prefix

--- config/urls.py
+++ config/urls.py
@@ -11,13 +11,13 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-    path('api/projects/', include('interfaces.project.urls')),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/projects', include('interfaces.project.urls')),
+    path('api/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
